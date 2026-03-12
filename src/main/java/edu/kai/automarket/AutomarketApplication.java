15d8fdbd4eb894426da528c40f2ccc8034cd6611
Munoon <munoongg@gmail.com>
2026-03-12 14:26:22 +0200

    Initial commit

---
+++ src/main/java/edu/kai/automarket/AutomarketApplication.java
@@ -0,0 +1,13 @@
+package edu.kai.automarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomarketApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AutomarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/kai/automarket/AutomarketApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kai.automarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
