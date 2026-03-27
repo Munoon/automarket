4e7e5c5b52f2b186c73073caa2be0e58844dc12b
munoon <munoongg@gmail.com>
2026-03-28 00:58:56 +0200

    Initialize svelte project

---
+++ svelte.config.js
@@ -0,0 +1,33 @@
+import adapter from '@sveltejs/adapter-static';
+import { relative, sep } from 'node:path';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	compilerOptions: {
+		// defaults to rune mode for the project, execept for `node_modules`. Can be removed in svelte 6.
+		runes: ({ filename }) => {
+			const relativePath = relative(import.meta.dirname, filename);
+			const pathSegments = relativePath.toLowerCase().split(sep);
+			const isExternalLibrary = pathSegments.includes('node_modules');
+
+			return isExternalLibrary ? undefined : true;
+		}
+	},
+	kit: {
+	    files: {
+            assets: 'src/main/svelte/static',
+            lib: 'src/main/svelte/lib',
+            routes: 'src/main/svelte/routes',
+            appTemplate: 'src/main/svelte/app.html'
+        },
+		adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+	}
+};
+
+export default config;
