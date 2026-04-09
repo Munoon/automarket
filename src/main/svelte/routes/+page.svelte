74cc75b6c7f3708377e61376e5d8782835d28496
munoon <munoongg@gmail.com>
2026-04-09 15:55:39 +0300

    Fix tab name not being restored on main page bug

--- src/main/svelte/routes/+page.svelte
+++ src/main/svelte/routes/+page.svelte
@@ -3,6 +3,10 @@
   import MyListingsStrip from './MyListingsStrip.svelte';
 </script>
 
+<svelte:head>
+  <title>automarket</title>
+</svelte:head>
+
 <div class="max-w-7xl mx-auto px-6 py-8 flex flex-col gap-0">
   <MyListingsStrip />
   <ListingsGrid />
